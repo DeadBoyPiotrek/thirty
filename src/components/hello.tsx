f87198ad00315720a3710981e8f75e744c6c6e13
Pio trek <pipolito2115@gmail.com>
2023-09-16 16:27:40 +0200

    feat: add prisma

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,16 +1,9 @@
 'use client';
+
 import { trpc } from '@/app/_trpc/client';
 
 export const Hello = () => {
-  const hello = trpc.hello.useQuery();
+  const { data: greeting, isLoading } = trpc.hello.useQuery();
 
-  if (hello.isError) {
-    return <div>Error: {hello.error.message}</div>;
-  }
-
-  if (hello.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return <div>{hello.data}</div>;
+  return isLoading ? 'Loading...' : <h1>{greeting}</h1>;
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,22 @@
+import { prisma } from './prisma';
 import { publicProcedure, router } from './trpc';
+import { z } from 'zod';
 
 export const appRouter = router({
   hello: publicProcedure.query(async () => {
-    return 'Hello World! 🌎';
+    return `Hello from server !`;
   }),
+  setName: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ input }) => {
+      const user = await prisma.user.create({
+        data: {
+          name: input.name,
+          email: input.name,
+        },
+      });
+      return user;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
